6f55842a7db02bea1305f35572141d27c94da9ff
subhasbucket <subhas.sing@gmail.com>
2024-04-23 17:49:20 -0700

    added bootstrap

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,17 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 const eleventySass = require("eleventy-sass");
 
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc,{
         useTransform: true,
     });
     eleventyConfig.addPlugin(eleventySass);
+
+    eleventyConfig.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+          'dd LLL yyyy'
+        );
+    })
 };
